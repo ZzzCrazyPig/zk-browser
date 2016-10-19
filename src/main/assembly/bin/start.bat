1ad4782de332042c5b86fb1f2adef1fabe1cce9c
ZzzCrazyPig <18825111236@163.com>
2016-10-19 14:33:12 +0800

    refactor deploy resources

--- src/main/assembly/bin/start.bat
+++ src/main/assembly/bin/start.bat
@@ -8,5 +8,16 @@ echo WARN: JAVA_HOME environment variable is not set.
 echo ---------------------------------------------------
 set "JAVA_CMD=java"
 :mainEntry
+set "argLine="
+:loop
+if "%1"=="" goto break
+set "argLine=%argLine% %1"
+shift
+goto loop
+:break
+set "CURR_DIR=%cd%"
+cd ..
+set "DIST_HOME=%cd%"
+cd %CURR_DIR%
 @echo on
-"%JAVA_CMD%" -jar ..\zk-browser.war
+"%JAVA_CMD%" -DDIST_HOME=%DIST_HOME% -cp "../conf;../lib/*" -jar ..\zk-browser.war %argLine%
--- src/main/assembly/bin/start.sh
+++ src/main/assembly/bin/start.sh
@@ -14,7 +14,20 @@ if [ -z "$JAVA_HOME" ] ; then
 		echo
 		exit 1
 	fi
+
+#set DIST_HOME
+CURR_DIR=`pwd`
+cd `dirname "$0"`/..
+DIST_HOME=`pwd`
+cd $CURR_DIR
+if [ -z "$DIST_HOME" ] ; then
+    echo
+    echo "Error: DIST_HOME environment variable is not defined correctly."
+    echo
+    exit 1
+fi
+
 RUN_CMD="\"$JAVA_HOME/bin/java\""
-RUN_CMD="$RUN_CMD -jar ../zk-browser.war
+RUN_CMD="$RUN_CMD -DDIST_HOME=\"$DIST_HOME\" -cp \"../conf:../lib/*\" -jar ../zk-browser.war ${1+$@}"
 #echo $RUN_CMD
 eval $RUN_CMD
